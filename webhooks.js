db72d87eb2115d3c5ff2d31922d254b21ccebb99
debbyji <jasmine820921@gmail.com>
2019-10-24 23:50:56 +0800

    covert indentation to spaces

--- webhooks.js
+++ webhooks.js
@@ -5,41 +5,41 @@ module.exports.sendReportToSlack = req => {
 
   if (!SLACK_WEBHOOK_URL) {
     return
-	}
+  }
 
-	const postData = {
-		"blocks": [
-			{
-				"type": "context",
-				"elements": [
-					{
-						"type": "image",
-						"image_url":`https://avatars0.githubusercontent.com/u/${req.user.githubId}?v=4`,
-						"alt_text": req.user.nickname
-					},
-					{
-						"type": "mrkdwn",
-						"text": `*<https://lidemy-learning-center.netlify.com/users/${req.user.id}|${req.user.nickname}>*`
-					}
-				]
-			},
-			{
-				"type": "section",
-				"text": {
-					"type": "mrkdwn",
-					"text": "```" + req.body.content + "```"
-				}
-			},
-			{
-				"type": "context",
-				"elements": [
-					{
-						"type": "mrkdwn",
-						"text": "*<https://lidemy-learning-center.netlify.com/| Go lidemy-learning-center>*"
-					}
-				]
-			}
-		]
+  const postData = {
+    "blocks": [
+      {
+        "type": "context",
+        "elements": [
+          {
+            "type": "image",
+            "image_url":`https://avatars0.githubusercontent.com/u/${req.user.githubId}?v=4`,
+            "alt_text": req.user.nickname
+          },
+          {
+            "type": "mrkdwn",
+            "text": `*<https://lidemy-learning-center.netlify.com/users/${req.user.id}|${req.user.nickname}>*`
+          }
+        ]
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "```" + req.body.content + "```"
+        }
+      },
+      {
+        "type": "context",
+        "elements": [
+          {
+            "type": "mrkdwn",
+            "text": "*<https://lidemy-learning-center.netlify.com/| Go lidemy-learning-center>*"
+          }
+        ]
+      }
+    ]
 }
 
   axios.post(SLACK_WEBHOOK_URL, postData)
