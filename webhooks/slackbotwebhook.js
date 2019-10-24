607b864397d92af8fda944973b90c4eaba217385
debbyji <jasmine820921@gmail.com>
2019-10-24 23:26:00 +0800

    use axios

--- webhooks/slackbotwebhook.js
+++ webhooks/slackbotwebhook.js
@@ -1,51 +1,53 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 module.exports = req => {
-  try {
-    fetch(`https://hooks.slack.com/services/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`, {
-      method: "POST",
-      headers: {
-        'Content-Type' : 'application/json',
-        'Content-Security-Policy' : "default-src 'self'; script-src https://hooks.slack.com/services/* "
-      },
-      body: JSON.stringify({
-        "blocks": [
-          {
-            "type": "context",
-            "elements": [
-              {
-                "type": "image",
-                "image_url":`https://avatars0.githubusercontent.com/u/${req.user.githubId}?v=4`,
-                "alt_text": req.user.nickname
-              },
-              {
-                "type": "mrkdwn",
-                "text": `*<https://lidemy-learning-center.netlify.com/users/${req.user.id}|${req.user.nickname}>*`
-              }
-            ]
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "```" + req.body.content + "```"
-            }
-          },
-          {
-            "type": "context",
-            "elements": [
-              {
-                "type": "mrkdwn",
-                "text": "*<https://lidemy-learning-center.netlify.com/| Go lidemy-learning-center>*"
-              }
-            ]
-          }
-        ]
-      })
-    });
-    console.log('success')
 
-  } catch (e) {
-    console.log('error', e)
+  if (!process.env.SLACK_WEBHOOK_URL) {
+    return
   }
+
+  axios({
+    method: 'post',
+    url: `https://hooks.slack.com/services/${process.env.SLACK_WEBHOOK_URL}`,
+    headers: {
+      'Content-Type' : 'application/json',
+      'Content-Security-Policy' : "default-src 'self'; script-src https://hooks.slack.com/services/* "
+    },
+    data: JSON.stringify({
+      "blocks": [
+        {
+          "type": "context",
+          "elements": [
+            {
+              "type": "image",
+              "image_url":`https://avatars0.githubusercontent.com/u/${req.user.githubId}?v=4`,
+              "alt_text": req.user.nickname
+            },
+            {
+              "type": "mrkdwn",
+              "text": `*<https://lidemy-learning-center.netlify.com/users/${req.user.id}|${req.user.nickname}>*`
+            }
+          ]
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "```" + req.body.content + "```"
+          }
+        },
+        {
+          "type": "context",
+          "elements": [
+            {
+              "type": "mrkdwn",
+              "text": "*<https://lidemy-learning-center.netlify.com/| Go lidemy-learning-center>*"
+            }
+          ]
+        }
+      ]
+    })
+  })
+  .then()
+  .catch(error => console.log(error))
 }
